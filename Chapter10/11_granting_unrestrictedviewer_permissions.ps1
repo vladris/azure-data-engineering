988d446584cd3df73abab42687d9cf2c1cad007a
Vlad Riscutia <vladris@outlook.com>
2021-04-17 07:43:01 -0700

    Fix spacing

--- Chapter10/11_granting_unrestrictedviewer_permissions.ps1
+++ Chapter10/11_granting_unrestrictedviewer_permissions.ps1
@@ -2,7 +2,7 @@ $me = az ad signed-in-user show --query objectId
 
 az kusto database-principal-assignment create `
 --cluster-name "adx$suffix" `
---database-name telemetry`
+--database-name telemetry `
 --principal-id $me `
 --principal-type User `
 --role UnrestrictedViewers `
