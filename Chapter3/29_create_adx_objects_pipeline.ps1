58fa5f518221965c6b7f3b0a04cc8ed5350b9d90
Vlad Riscutia <vladris@outlook.com>
2021-05-03 11:33:49 -0700

    Update 29_create_adx_objects_pipeline.ps1

--- Chapter3/29_create_adx_objects_pipeline.ps1
+++ Chapter3/29_create_adx_objects_pipeline.ps1
@@ -2,4 +2,10 @@ git add *
 git commit -m "ADX objects pipeline"
 git push
 
-az pipelines create --name 'Deploy ADX objects' --repository DE --repository-type tfsgit --yml-path YML/deploy-adx-objects.yml --skip-run
+az pipelines create `
+--name 'Deploy ADX objects' `
+--repository DE `
+--repository-type tfsgit `
+--branch master `
+--yml-path YML/deploy-adx-objects.yml `
+--skip-run
